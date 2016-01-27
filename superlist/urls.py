d33bc1156e190e95f7caac1101de043958113dc8
welldoer <welldoer@263.net>
2016-01-28 00:22:27 +0800

    第一个单元测试通过，完成URL映射

--- superlist/urls.py
+++ superlist/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'superlist.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'lists.views.home_page', name='home'),
 )
