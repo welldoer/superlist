4d64516ea021978017e072b8c842bb20cb7012a0
welldoer <welldoer@263.net>
2016-01-29 04:47:37 +0800

    采用新的URL、视图、模板来显示用户任务清单

--- superlist/urls.py
+++ superlist/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
 )
