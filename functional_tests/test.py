4c37b1632e199c266cf488f68dcbfcf639e92b04
welldoer <welldoer@263.net>
2016-01-29 16:06:24 +0800

    添加任务记录到现有的清单中

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -34,14 +34,15 @@ class NewVisitorTest(LiveServerTestCase):
 
     edith_list_url = self.browser.current_url
     self.assertRegex(edith_list_url, '/lists/.+')
+
     self.check_for_row_in_list_table('1: Buy peacock feathers')
 
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
 
-    self.check_for_row_in_list_table('1: Buy peacock feathers')
     self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    self.check_for_row_in_list_table('1: Buy peacock feathers')
 
     self.browser.quit()
     self.browser = webdriver.Firefox()
@@ -62,5 +63,3 @@ class NewVisitorTest(LiveServerTestCase):
     page_text = self.browser.find_element_by_tag_name('body').text
     self.assertNotIn('Buy peacock feathers', page_text)
     self.assertIn('Buy milk', page_text)
-
-    self.fail('Finish the test!')
--- superlist/urls.py
+++ superlist/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
 
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
 )
